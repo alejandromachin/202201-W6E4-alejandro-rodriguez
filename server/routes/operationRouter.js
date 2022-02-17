a2dcdfd6617c1a5020b4ff8638be5fb67c550f63
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-17 14:59:19 +0100

    added resta function to path

--- server/routes/operationRouter.js
+++ server/routes/operationRouter.js
@@ -4,7 +4,7 @@ const debug = require("debug")("calculator:root");
 const { response } = require("express");
 const express = require("express");
 // const { port } = require("../../index");
-const { suma } = require("../../utils/operators");
+const { suma, resta } = require("../../utils/operators");
 
 const router = express.Router();
 
@@ -23,6 +23,24 @@ router.get("/suma", (req, res) => {
   const resultadoSuma = suma(a, b);
 
   res.json({ suma: resultadoSuma });
+
+  response.end();
+});
+router.get("/resta", (req, res) => {
+  debug(`Resquest arrived at ${req.url} with method ${req.method}`);
+  const urlCalculator = new URL(`http://localhost:3001${req.url}`);
+
+  const variables = [];
+  urlCalculator.searchParams.forEach((variable) => {
+    variables.push(variable);
+  });
+
+  const a = variables[0];
+  const b = variables[1];
+
+  const resultadoResta = resta(a, b);
+
+  res.json({ resta: resultadoResta });
 
   response.end();
 });
